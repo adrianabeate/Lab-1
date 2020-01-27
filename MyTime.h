d01fbe851e6192b5293e5eed6a27f53d5b8f5193
adrianabeate <50586703+adrianabeate@users.noreply.github.com>
2020-01-27 10:53:41 -0500

    Update MyTime.h

--- MyTime.h
+++ MyTime.h
@@ -15,23 +15,68 @@ public:
 
     void Reset(int h, int m);
 
-    MyTime operator + (const MyTime& t1, const MyTime& t2);
+    MyTime MyTime::operator + ( const MyTime& t2)const
+    {   MyTime tmp;
 
-    MyTime operator - (const MyTime& t1, const MyTime& t2);
+	tmp.hours = hours + t2.hours;
+	tmp.minutes = minutes + t2.minutes;
 
-    MyTime operator * (const MyTime& t1, int num);
+	tmp.simplify();
 
-    MyTime operator / (const MyTime& t1, int num);
+	return tmp;
+    }
+
+    MyTime operator - (const MyTime& t1, const MyTime& t2)
+    {MyTime tmp;
+
+	tmp.minutes = abs((t1.hours * 60 + t1.minutes) -
+
+		(t2.hours * 60 + t2.minutes));
+
+	tmp.simplify();
+
+	return tmp;
+    }
+
+    MyTime operator * (const MyTime& t1, int num)
+    {MyTime tmp;
+
+	tmp.minutes = t1.hours * 60 + t1.minutes;
+
+	tmp.minutes *= num;
+
+	tmp.simplify();
+
+	return tmp;
+    }
+
+    MyTime operator / (const MyTime& t1, int num)
+    {	MyTime tmp;
+
+	tmp.minutes = t1.hours * 60 + t1.minutes;
+
+	tmp.minutes /= num;
+
+	tmp.simplify();
+
+	return tmp;
+    }
 
     std::istream& operator >>(std::istream& fin, MyTime& t);
 
     std::ostream& operator <<(std::ostream& fout, const MyTime& t);
 
-    bool operator == (const MyTime& t1, const MyTime& t2);
+    bool operator == (const MyTime& t1, const MyTime& t2)
+    { return t1.hours == t2.hours && t1.minutes == t2.minutes;
+    }
 
-    bool operator < (const MyTime& t1, const MyTime& t2);
+    bool operator < (const MyTime& t1, const MyTime& t2)
+    {return (t1.hours * 60 + t1.minutes) < (t2.hours * 60 + t2.minutes);
+    }
 
-    bool operator <= (const MyTime& t1, const MyTime& t2);
+    bool operator <= (const MyTime& t1, const MyTime& t2)
+    {return (t1.hours * 60 + t1.minutes) <= (t2.hours * 60 + t2.minutes);
+    }
 
     void input(std::istream& ins);
 
@@ -46,4 +91,6 @@ private:
     int minutes;   // 0 <= minutes <= 59
 };
 
+ std::istream& operator >>(std::istream& fin, MyTime& t);
 
+    std::ostream& operator <<(std::ostream& fout, const MyTime& t);
